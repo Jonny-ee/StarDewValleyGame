6ff8487e0ea54a481b39ae6e4f550a676f4edfed
Jonathan Zheng <jonathanar003@gmail.com>
2024-12-10 22:20:47 +0800

    合并代码，增加传送功能

--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -4,28 +4,42 @@
 #include "GameMap.h"
 #include <set>
 
+/*
+ * ��Ϸ������
+ * ���ܣ�������Ϸ������
+ * 1. ��ͼ���غ���ʾ
+ * 2. ��Ҷ������
+ * 3. �����л�
+ * 4. ���봦��
+ * 5. �������
+ */
 class GameScene : public cocos2d::Scene
 {
 public:
-    static cocos2d::Scene* createScene();
-    virtual bool init();
-
+    static cocos2d::Scene* createScene();  // ��������
+    virtual bool init();                   // ��ʼ������
     CREATE_FUNC(GameScene);
+    virtual void update(float dt);         // �������º���
 
-    // 更新函数
-    virtual void update(float dt);
+    // �����л����
     void switchToMap(const std::string& mapName, const cocos2d::Vec2& targetTilePos);
-    // 键盘事件处理函数
-    void onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
-    void onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
     void switchToFarmMap();
-    //相机
+
+    // �������
     void updateCamera();
 
+    // ���봦��
+    void onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
+    void onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
+
 private:
-    Player* player = nullptr;
-    GameMap* _gameMap = nullptr;
-    std::set<char> _pressedKeys;
-    // 添加成员变量来跟踪监听器状态
-    cocos2d::EventListenerKeyboard* _keyboardListener = nullptr;
+    // ��Ϸ����
+    Player* player = nullptr;                             // ��Ҷ���ָ��
+    GameMap* _gameMap = nullptr;                         // �µ�ͼϵͳ
+    cocos2d::TMXTiledMap* tileMap = nullptr;            // ��ͳ��ͼϵͳ
+    cocos2d::TMXObjectGroup* collisionsGroup = nullptr;  // ��ײ��
+
+    // ����״̬
+    std::set<char> _pressedKeys;                         // �Ѱ��µİ�������
+    cocos2d::EventListenerKeyboard* _keyboardListener = nullptr;  // ���̼�����
 };
