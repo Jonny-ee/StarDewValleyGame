a0fa629b2da34cd6a5e7b5d2ba1835f5ab8f7186
XiangMaster <2744253823@qq.com>
2024-12-12 02:28:53 +0800

    人物对话交互完成，对话功能完善，鼠标样式及其还原后续换素材

--- Classes/DialogueBox.h
+++ Classes/DialogueBox.h
@@ -1,12 +1,16 @@
 #pragma once
 #include "cocos2d.h"
+#include <vector>
+#include <cstdlib>
 
 class DialogueBox : public cocos2d::Node
 {
 public:
     static DialogueBox* create(const std::string& dialogue, const std::string& characterImagePath);
     virtual bool init(const std::string& dialogue, const std::string& characterImagePath);
+    
 private:
     void startTypingEffect(cocos2d::Label* label, const std::string& dialogue);// 打字效果
-    void close();
+    void close();// 关闭
+    
 };
