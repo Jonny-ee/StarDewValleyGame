23906ca0e4331eaf7a49bbe63081f2ca7ac71210
XiangMaster <2744253823@qq.com>
2024-12-12 02:29:19 +0800

    人物对话交互完成，对话功能完善，鼠标样式及其还原后续换素材

--- Classes/DialogueBox.cpp
+++ Classes/DialogueBox.cpp
@@ -59,27 +59,46 @@ bool DialogueBox::init(const std::string& dialogue, const std::string& character
 
     // 创建角色头像
     auto characterImage = Sprite::create("Portraits/Lewis.png");
-    characterImage->setPosition(Vec2(70, 4)); // 根据需要调整位置
+    characterImage->setPosition(Vec2(70, 6)); // 根据需要调整位置
     characterImage->setTextureRect(cocos2d::Rect(0, 0, 64, 64));
-    characterImage->setScale(0.45f);
+    characterImage->setScale(0.4f);
     this->addChild(characterImage);
 
     // 创建姓名文本
-    auto nameLabel = Label::createWithSystemFont("Lewis", "Arial", 6);//后面新NPC要改
+    auto nameLabel = Label::createWithSystemFont("Lewis", "Times New Roman", 10);//后面新NPC要改
     nameLabel->setTextColor(cocos2d::Color4B(0, 0, 0, 255)); // 黑色，RGBA格式
-    nameLabel->setPosition(Vec2(70, -13)); // 根据需要调整位置
+    nameLabel->setPosition(Vec2(70, -11)); // 根据需要调整位置
     this->addChild(nameLabel);
 
     // 创建对话文本
     auto label = Label::createWithSystemFont(dialogue, "Arial", 10);
-    
+    label->setDimensions(120, 0); // 设置最大宽度并启用自动换行
+    label->setLineBreakWithoutSpace(true); // 不允许在单词中间换行
     this->addChild(label);
     // 开始打字效果
     label->setTextColor(cocos2d::Color4B(0, 0, 0, 255)); // 黑色，RGBA格式
-    label->setPosition(Vec2(-40, 8)); // 根据需要调整位置
+    label->setPosition(Vec2(-20, 0)); // 根据需要调整位置
     startTypingEffect(label, dialogue);
+
+    // 添加关闭按钮
+    auto closeButton = MenuItemImage::create("LooseSprites/shadow.png", "LooseSprites/shadow.png", [this](Ref* sender) {
+        this->close(); // 点击关闭按钮时关闭对话框
+        });
+    closeButton->setPosition(Vec2(90, 17)); // 根据需要调整位置
+
+    auto menu = Menu::create(closeButton, nullptr);
+    menu->setPosition(Vec2::ZERO); // 设置菜单位置为(0, 0)
+    this->addChild(menu);
+
+    // 添加鼠标监听器
+    auto mouseListener = EventListenerMouse::create();
+    mouseListener->onMouseDown = [this](Event* event) {
+        this->close(); // 点击时关闭对话框
+        };
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(mouseListener, this);
+
     // 添加出现动画
-    this->setScale(0.1f); // 初始缩放为0.1
+    this->setScale(3.7f); // 初始缩放为3.7
     auto appearAction = ScaleTo::create(0.3f, 4.0f); // 0.3秒内缩放到4.0
     auto fadeInAction = FadeIn::create(0.3f); // 0.3秒内淡入
     auto spawnAction = Spawn::create(appearAction, fadeInAction, nullptr); // 同时执行缩放和淡入
@@ -87,6 +106,7 @@ bool DialogueBox::init(const std::string& dialogue, const std::string& character
     
     return true;
 }
+
 void DialogueBox::close()
 {
     // 添加关闭动画
