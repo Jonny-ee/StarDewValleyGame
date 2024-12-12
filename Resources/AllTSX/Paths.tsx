56b5cffceb3fb5646275a7a1c37e4de730600736
Jonathan Zheng <jonathanar003@gmail.com>
2024-12-13 01:14:42 +0800

    为Farm瓦片添加属性，增加对象层

--- Resources/AllTSX/Paths.tsx
+++ Resources/AllTSX/Paths.tsx
@@ -1,4 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="Paths" tilewidth="16" tileheight="16" tilecount="64" columns="4">
  <image source="../maps/Paths.png" width="64" height="256"/>
+ <tile id="9">
+  <properties>
+   <property name="Order" value="TreeReborn"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="Order" value="StoneReborn"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="Order" value="GrassReborn"/>
+  </properties>
+ </tile>
 </tileset>
--- Resources/AllTSX/spring_outdoorsTileSheet.tsx
+++ Resources/AllTSX/spring_outdoorsTileSheet.tsx
@@ -1,4 +1,164 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="spring_outdoorsTileSheet" tilewidth="16" tileheight="16" tilecount="1975" columns="25">
  <image source="../maps/spring_outdoorsTileSheet.zh-CN.png" width="400" height="1264"/>
+ <tile id="1227">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1228">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1229">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1230">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1231">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1247">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1248">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1249">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1252">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1253">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1254">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1255">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1256">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1272">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1273">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1274">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1277">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1278">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1279">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1280">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1281">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1297">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1298">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1299">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1302">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1303">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1304">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1305">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1306">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1322">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1323">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
+ <tile id="1324">
+  <properties>
+   <property name="MapType" value="Water"/>
+  </properties>
+ </tile>
 </tileset>
