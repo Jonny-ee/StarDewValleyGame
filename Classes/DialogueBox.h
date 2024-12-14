b4fe22243de2e396fac15f473dc9fd45b9531bef
XiangMaster <2744253823@qq.com>
2024-12-15 03:33:12 +0800

    1.对话框跟随镜头  2.新增NPC：Marlon  Maru  Alex

--- Classes/DialogueBox.h
+++ Classes/DialogueBox.h
@@ -6,11 +6,10 @@
 class DialogueBox : public cocos2d::Node
 {
 public:
-    static DialogueBox* create(const std::string& dialogue, const std::string& characterImagePath);
-    virtual bool init(const std::string& dialogue, const std::string& characterImagePath);
-    
+    static DialogueBox* create(const std::string& dialogue, const std::string& characterImagePath, const std::string& characterName);
+    virtual bool init(const std::string& dialogue, const std::string& characterImagePath, const std::string& characterName);
+
 private:
     void startTypingEffect(cocos2d::Label* label, const std::string& dialogue);// 打字效果
     void close();// 关闭
-    
 };
