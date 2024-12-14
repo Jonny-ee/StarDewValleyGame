b2cf8308c29016a6991bbb9670317b624b090792
XiangMaster <2744253823@qq.com>
2024-12-15 03:32:23 +0800

    1.对话框跟随镜头  2.新增NPC：Marlon  Maru  Alex

--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -3,6 +3,9 @@
 #include "Player.h"
 #include "GameMap.h"
 #include "Lewis.h"
+#include "Marlon.h"
+#include "Maru.h"
+#include "Alex.h"
 #include "InventoryUI.h"
 #include "FishingSystem.h"
 
@@ -42,6 +45,13 @@ private:
     Player* player = nullptr;                             // 玩家对象指针
     GameMap* _gameMap = nullptr;                          // 游戏地图指针
     Lewis* lewis = nullptr;                               // Lewis指针
+    bool isLewisCreated = false;                          // 添加标志以跟踪刘易斯是否已创建
+    Marlon* marlon = nullptr;                             // Marlon指针
+    bool isMarlonCreated = false;                          // 添加标志以跟踪马龙是否已创建
+    Maru* maru = nullptr;                                 // Maru指针
+    bool isMaruCreated = false;                             // 添加标志以跟踪玛鲁是否已创建
+    Alex* alex = nullptr;                                 // Alex指针
+    bool isAlexCreated = false;                             // 添加标志以跟踪艾利克斯是否已创建
     DialogueBox* dialogueBox = nullptr;                   // 对话框指针
     cocos2d::TMXTiledMap* tileMap = nullptr;              // Tiled 地图系统
     cocos2d::TMXObjectGroup* collisionsGroup = nullptr;   // 碰撞组
@@ -49,6 +59,10 @@ private:
     cocos2d::Sprite* toolIcon = nullptr;  // 工具图标
     void initToolIcon();                  // 初始化工具图标
     void updateToolIcon();                // 更新工具图标
+    void initLewis();                //初始化Lewis
+    void initMarlon();            // 初始化马龙
+    void initMaru();            // 初始化玛鲁
+    void initAlex();            // 初始化艾利克斯
     std::vector<Chest*> _chests;     // 存储所有宝箱
     void initChests();               // 初始化宝箱
 
