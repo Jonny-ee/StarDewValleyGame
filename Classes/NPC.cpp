71576072a700304ae87a531c5b9b15a098e4df29
XiangMaster <2744253823@qq.com>
2024-12-18 03:37:49 +0800

    更新动物：猪、羊、鸡  增加手持物 胡萝卜 增加动物喂养互动

--- Classes/NPC.cpp
+++ Classes/NPC.cpp
@@ -218,7 +218,26 @@ void NPC::updateRelationshipStatus()
     // 根据好感度更新关系状态
     // 可以添加更多关系相关的逻辑
 }
-
+void NPC::showFull()
+{
+    // 初始化表情
+    auto emoji = Sprite::create("TileSheets/emotes.png");
+    if (emoji) {
+        emoji->setTextureRect(cocos2d::Rect(0, 80, 16, 16));
+        emoji->setScale(1.0f);
+        emoji->setAnchorPoint(Vec2(-1, -1));  // 设置emoji的锚点为左下角
+        emoji->setVisible(true);
+        this->addChild(emoji);
+    }
+    // 创建延迟动作，2秒后恢复状态
+    auto delay = DelayTime::create(2.0f);
+    auto resetAction = CallFunc::create([emoji]() {
+        emoji->setVisible(false);  // 2秒后将emoji隐藏
+        });
+    // 将延迟动作和重置动作结合
+    auto sequence = Sequence::create(delay, resetAction, nullptr);
+    this->runAction(sequence); // 运行动画
+}
 
 
 
