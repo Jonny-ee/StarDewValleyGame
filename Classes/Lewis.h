5fb026b5c42b8021ff6ee0704831f865bf77e416
XiangMaster <2744253823@qq.com>
2024-12-22 15:14:22 +0800

    新增挖矿系统、新增NPC好感度与关系建立系统

--- Classes/Lewis.h
+++ Classes/Lewis.h
@@ -6,42 +6,34 @@
 #include "QuestSystem.h"
 #include <vector>
 
-
 //刘易斯类
 class Lewis : public NPC
 {
 public:
     static Lewis* create();
-    virtual bool init();
+    virtual bool init() override;
     void moveToDirection(cocos2d::Vec2& destination, float dt);//移动到目的地
-    void staticAnimation();//人物静止时的呼吸动画
+    void staticAnimation() override;//人物静止时的呼吸动
     void moveAlongPath(float dt);//移动沿路径
 
-   // void startConversation();
+   // void startConversation() override;
     std::vector<cocos2d::Vec2> path; // 移动路径
-    std::string getRandomDialogue();
-
+   
     void showThanks();//感谢动画
     void setActionState(ActionState state) { currentActionState = state; }
 
-    // 检查是否有可接任务
-    bool hasActiveQuest() const;
-
-    // 显示任务对话
-    void showQuestDialogue(GameScene* scene);
 private:
     void initializeDefaultBehavior();  // 初始化刘易斯的默认行为
-    void loadLewisDialogues() noexcept;         // 加载刘易斯专属对话
-    std::vector<std::string> dialogueList = {
-        "Welcome to Pelican Town!",
-        "As a mayor, I have been working hard to make this town a better place.",
-        "You have made great contributions to Peican Town!",
-        "You have become an indispensable part of this small town!"
-    };
-    
-    int currentPathIndex = 0; // 当前路径索引
+    void completeDialogue()
+    {
+        this->addDialogue("Welcome to Pelican Town!");
+        this->addDialogue("As a mayor, I have been working hard to make this town a better place.");
+        this->addDialogue("You have made great contributions to Peican Town!");
+        this->addDialogue("You have become an indispensable part of this small town!");
+    }
+    std::function<void(const std::string&, const std::string&, const std::string&, int)> friendshipCallback; // 友谊回调
 
-    
+    int currentPathIndex = 0; // 当前路径索引
 
     ActionState currentActionState = ActionState::MOVING; // 当前动作状态
     float waitTime = 2.0f; // 等待时间
